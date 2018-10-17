421ca1e4a6f1b6c7c4f17d71a6c18616b71720e5
epauser <epauser@localhost.localdomain>
2018-10-17 21:20:10 +0100

    Update

--- Week3/cpu_count.sh
+++ Week3/cpu_count.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# cnt variable
 cnt=$(grep processor /proc/cpuinfo | wc -l)
-if [ -n "$1" ]; then 
+
+# if the lenght of the string is greater than zero -n
+if [ -n "$1" ]; then
+	#if CPU is less than paramter passed print
 	if [ $cnt -lt $1 ]; then
   		echo "Insufficient CPUs, install abandoned"
 	fi
+# if the lenght of the string is not greater than zero, then print	
 else
 	echo "Param not entered"
 fi
