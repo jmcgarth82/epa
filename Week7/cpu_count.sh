987715e2809af5d2a2051a36fbe10f63c95f83a6
epauser <epauser@localhost.localdomain>
2018-11-14 21:15:01 +0000

    Docker week7

---
+++ Week7/cpu_count.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This file contains information
+# on the number of cores on the VM
+# /proc/cpuinfo
+
+# grep is used to search for a string
+
+# pipe | is used to join the output of
+# one comand as input to another
+
+
+# `` executes the command and we store
+# the result of the command in cnt
+
+cnt=`grep rocessor /proc/cpuinfo | wc -l`
+echo "The number of cores is " $cnt
+
